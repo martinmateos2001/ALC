0d7ac0512cae70270a6ad4e025031f4c49842632
Martin Mateos <martinmateos_2001@hotmail.com>
2026-08-29 00:11:23 -0300

    Copiados lista de ejercicios a entregar.

--- Laboratorio/alc_1_2.py
+++ Laboratorio/alc_1_2.py
@@ -1,1 +1,57 @@
 import numpy as np
+# --- Laboratorio 1 ---
+def error(x,y):
+    return 1
+
+'''
+Recibe dos numeros x e y, y calcula el error de aproximar x usando y en float64
+'''
+
+def error_relativo(x,y):
+    return 1
+
+'''
+Recibe dos numeros x e y, y calcula el error relativo de aproximar x usando y en float649
+'''
+
+def matricesIguales(A,B):
+    return True
+
+'''
+Devuelve True si ambas matrices son iguales y False en otro caso.
+Considerar que las matrices pueden tener distintas dimensiones, ademas de distintos valores.
+'''
+
+# --- Laboratorio 2 ---
+def rota(theta):
+    return None
+"""
+Recibe un ángulo theta y retorna una matriz de 2x2 que rota un vector dado en un ángulo theta
+"""
+
+def escala(s):
+    return None
+'''
+Recibe una tira de números s y retorna una matriz cuadrada de n x n, donde n es el tamaño de s.
+La matriz escala la componente i de un vector de Rn en un factor s[i]
+'''
+
+def rota_y_escala(theta, s):
+    return None
+'''
+Recibe un ángulo theta y una tira de numeros s, y retorna una matriz 2x2 que rota el vector en un ángulo theta y luego los escala en un factor s.
+'''
+
+def afin(theta, s, b):
+    return None
+'''
+Recibe un angulo theta, una tira de numeros s (en R2) y un vector b en R2.
+Retorna una matriz 3x3 que rota el vector en un angulo theta, luego lo escala en un factor de s y por ultimo lo mueve en un factor fijo b. 
+'''
+
+def trans_afin(v, theta, s, b):
+    return None
+'''
+Recibe un vector v en R2, un angulo theta, una tira de numeros s en R2, y un vector b en R2.
+Retorna el vector w resultante de aplicar la transformacion afin a v.
+'''
